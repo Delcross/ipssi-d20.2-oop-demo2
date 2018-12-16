a5b56b9dcee5bbbbc33441fc5b86794ddc3ea0b8
Delcross <43371445+Delcross@users.noreply.github.com>
2018-12-16 23:52:19 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
+#!/usr/bin/php
 <?php
 
-use Zend\Diactoros\ServerRequestFactory;
+use Support\Application;
 
+if (!is_file(__DIR__.'/vendor/autoload.php')) {
+    throw new LogicException('The autoload file does not exist, please use composer install');
+}
 require __DIR__.'/vendor/autoload.php';
 
-//$request = ServerRequestFactory::fromGlobals();
-$request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
-
-$repository = new \InvoiceApp\Repository\Invoice\InMemory();
-$action = new \InvoiceApp\Action\Invoice\Listing($repository);
-$response = $action->handle($request);
-
-foreach ($response->getHeaders() as $key => $value) {
-    $value = implode(', ', $value);
-    header("$key: $value");
-}
-echo $response->getBody();
+echo (new Application($argv, require __DIR__.'/config/di.global.php'))->run();
